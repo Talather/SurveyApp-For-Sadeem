9dce0ed00b18c980d375e69edab4f87529fc2d8c
Sadeem Asim <hassan-rasheed>
2023-11-10 02:24:48 +0500

    sad

--- inspireAppModels/connect.js
+++ inspireAppModels/connect.js
@@ -13,10 +13,8 @@ const surveyinvitationsmodels = require("./surveyinvitations");
 const CompanyModels = require("./company");
 
 async function connectdb() {
-  // await mongoose.connect("mongodb://localhost:27017/Survey-App");
-  await mongoose.connect(
-    "mongodb+srv://junaidpluton:pluton123@livedb.iedy1.mongodb.net/surveyApp?retryWrites=true&w=majority"
-  );
+  await mongoose.connect("mongodb://localhost:27017/Survey-App");
+
   console.log("db connected");
 }
 module.exports = connectdb;
