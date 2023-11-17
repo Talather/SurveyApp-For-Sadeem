3ca2dd26e2c245b0f3ce7b4286eb717a9471c3c5
Talather <atherjawad11@gmail.com>
2023-11-17 16:13:41 +0500

    Topic Api completed

--- Middleware/authStatus.js
+++ Middleware/authStatus.js
@@ -27,63 +27,61 @@ exports.authMiddleware = (req, res, next) => {
 //If neither the access token nor the refresh token is provided, it returns an error message.
 exports.isAdminAuthenticated = (req, res, next) => {
   console.log("middleware executed")
-  console.log("delete fucntion middleware")
   const accessToken =
     req.header("x-access-token") || req.cookies["x-access-token"]
   // const accessToken = req.headers['authorization'];
   if (accessToken) {
     console.log("accessToken is provided")
   }
-  const refreshToken =
-    req.header("x-refresh-token") || req.cookies["x-refresh-token"]
-  if (refreshToken) {
-    console.log("refreshToken is provided")
-  }
+  // const refreshToken =
+  //   req.header("x-refresh-token") || req.cookies["x-refresh-token"]
+  // if (refreshToken) {
+  //   console.log("refreshToken is provided")
+  // }
   const secretKey = "Survey"
 
-  if (!accessToken && !refreshToken) {
-    console.log("access denied")
-    return res.status(401).send("Access Denied. No token provided.")
+  // if (!accessToken && !refreshToken) {
+  if (!accessToken) console.log("access denied.no access token is provided")
+  return res.status(401).send("Access Denied. No token provided.")
+}
+
+try {
+  req.user = jwt.verify(accessToken, secretKey)
+
+  console.log("access token verified")
+
+  if (req.user.role !== "admin") {
+    return res.status(401).send("Access denied")
   }
+  next()
+} catch (error) {
+  //   if (!refreshToken) {
+  //     console.log("oo lala")
+  //     return res.status(401).send("Access Denied. No refresh token provided.")
+  //   }
 
-  try {
-    req.user = jwt.verify(accessToken, secretKey)
+  //   try {
+  //     const decoded = jwt.verify(refreshToken, secretKey)
+  //     const payload = {
+  //       _id: decoded._id,
+  //       name: decoded.name,
+  //       email: decoded.email,
+  //       role: decoded.role,
+  //       ref: "AT101010",
+  //     }
+  //     const accessToken = jwt.sign(payload, secretKey, { expiresIn: "1h" })
 
-    console.log("access token verified")
-
-    if (req.user.role !== "admin") {
-      return res.status(401).send("Access denied")
-    }
-    next()
-  } catch (error) {
-    if (!refreshToken) {
-      console.log("oo lala")
-      return res.status(401).send("Access Denied. No refresh token provided.")
-    }
-
-    try {
-      const decoded = jwt.verify(refreshToken, secretKey)
-      const payload = {
-        _id: decoded._id,
-        name: decoded.name,
-        email: decoded.email,
-        role: decoded.role,
-        ref: "AT101010",
-      }
-      const accessToken = jwt.sign(payload, secretKey, { expiresIn: "1h" })
-
-      res
-        .cookie("x-access-token", accessToken, {
-          httpOnly: true,
-          maxAge: 365 * 24 * 60 * 60 * 1000,
-        })
-        .header("x-access-token", accessToken)
-        .header("x-refresh-token", refreshToken)
-        .send(decoded)
-    } catch (error) {
-      return res.status(400).send("Invalid Token.")
-    }
-  }
+  //     res
+  //       .cookie("x-access-token", accessToken, {
+  //         httpOnly: true,
+  //         maxAge: 365 * 24 * 60 * 60 * 1000,
+  //       })
+  //       .header("x-access-token", accessToken)
+  //       .header("x-refresh-token", refreshToken)
+  //       .send(decoded)
+  //   } catch (error) {
+  return res.status(400).send("Invalid Token.")
+  //   }
 }
 
 exports.isEmployeeAuthenticated = (req, res, next) => {
--- controllers/surveyCreation.controller.js
+++ controllers/surveyCreation.controller.js
@@ -8,41 +8,40 @@ const Survey = require("../Inspire_App_Models/survey")
 const Question = require("../Inspire_App_Models/question")
 const Answer = require("../Inspire_App_Models/answer")
 const Questions = require("../Inspire_App_Models/question")
-const Ajv = require("ajv")
+const val = require("ajv")
 
 // create survey
 exports.createSurvey = catchAsync(async (req, res, next) => {
-  const { name, description } = JSON.parse(req.body)
-  const questions = JSON.parse(req.body.questions)
-  const answers = JSON.parse(req.body.answers)
-  const QuestionNode = [{}]
-  for (const i in questions) {
-    QuestionNode[key] = []
-  }
-  const surveyExist = await Order.findOne({ paymentInfo })
+  const { name, description, questions, answers } = req.body
+  // const questions = JSON.parse(req.body.questions)
+  // const answers = JSON.parse(req.body.answers)
+  // const QuestionNode = [{}]
+  // for (const i in questions) {
+  //   QuestionNode[key] = []
+  // }
+  const surveyExist = await Survey.findOne({ name })
 
   if (surveyExist) {
     return next(new ErrorHandler("Survey Already Placed", 400))
   }
 
   //To validate the parsed version of data
-  const questionSchema = {
-    name: { type: "string", required: false },
-    description: [{ type: "string", required: false }],
-  }
-  const isValid = Ajv.validate(questionSchema, parsedQuestion)
-  if (!isValid) {
-    return res.status(400).send("Invalid question format")
-  }
+  // const questionSchema = {
+  //   name: { type: "string", required: false },
+  //   description: [{ type: "string", required: false }],
+  // }
+  // const isValid = validate(questionSchema, parsedQuestion)
+  // if (!isValid) {
+  //   return res.status(400).send("Invalid question format")
+  // }
 
   const surveyData = { name, description, questions, answers }
   const createdSurvey = await Survey.create(surveyData)
   //Create a Question Document for that survey document so that it could be refernced
   questionData = { survey: createdSurvey._id, questions }
-  addQuestionDocument = await Questions.create(questionData)
-
+  // addQuestionDocument = await Questions.create(questionData)
   //Create a answer Document for that survey document so that it could be refernced
-  addAnswerDocument = await Answer.create(answers)
+  // addAnswerDocument = await Answer.create(answers)
 
   res.status(200).json({
     success: true,
